d6ff27bf5b11568bb687dbdf807819b49e3ee81e
RUBALBHULLAR <69098507+RUBALBHULLAR@users.noreply.github.com>
2020-11-26 21:42:20 -0700

    update employee_db

--- EmployeeSQL/employee_queries.sql
+++ EmployeeSQL/employee_queries.sql
@@ -25,14 +25,33 @@ JOIN employees
 ON dept_manager.emp_no = employees.emp_no;
 
 
-
--- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+-- 4. List the department of each employee with the following information: employee number,
+--    last name, first name, and department name.
 SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
 FROM dept_emp
 JOIN employees
 ON dept_emp.emp_no = employees.emp_no
 JOIN departments
 ON dept_emp.dept_no = departments.dept_no;
+
+
+-- 5. List all employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name,sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
